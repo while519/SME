939d54654e5b3bad1bb5e3b1afaa4fc9191d7fae
while519 <wtyj891@gmail.com>
2016-05-23 10:55:07 +0100

    Update FB15k_Unstructured.py
    modify path

--- FB15k/FB15k_Unstructured.py
+++ FB15k/FB15k_Unstructured.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python
-from FB15k_exp import *
+import sys
 sys.path.append('/volatile/yuwu/git-repo/SME')
+from FB15k_exp import *
+
 
 launch(op='Unstructured', simfn='Dot', ndim=50, nhid=50, marge=1., lremb=0.01,
     lrparam=0.01, nbatches=100, totepochs=1000, test_all=1, neval=1000,
