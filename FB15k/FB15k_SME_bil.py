ea161300dafac99fa3776f2d083cea8c1d987edd
while519 <wtyj891@gmail.com>
2016-05-23 10:53:56 +0100

    Update FB15k_SME_bil.py
    modify path

--- FB15k/FB15k_SME_bil.py
+++ FB15k/FB15k_SME_bil.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python
-from FB15k_exp import *
+import sys
 sys.path.append('/volatile/yuwu/git-repo/SME')
+from FB15k_exp import *
+
 
 launch(op='SME_bil', simfn='Dot', ndim=50, nhid=50, marge=1., lremb=0.01,
     lrparam=1., nbatches=100, totepochs=1000, test_all=10, neval=1000,
