b4a091d874fc3f679c6eb492ea168a7f388676cb
yuwu <wtyj891@gmail.com>
2016-05-25 10:57:58 +0100

    done

--- FB15k/FB15k_SME_bil.py
+++ FB15k/FB15k_SME_bil.py
@@ -2,8 +2,10 @@
 import sys
 sys.path.append('/volatile/yuwu/git-repo/SME')
 from FB15k_exp import *
-
+from  FB15k_evaluation import *
 
 launch(op='SME_bil', simfn='Dot', ndim=50, nhid=50, marge=1., lremb=0.01,
     lrparam=1., nbatches=100, totepochs=1000, test_all=10, neval=1000,
     savepath='FB15k_SME_bil', datapath='../data/')
+
+RankingEval(loadmodel='FB15k_SME_bil')
